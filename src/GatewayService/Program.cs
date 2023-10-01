6f609c1767522a464a06b6589f8a2b1c41e46c93
lak <caizh1997@live.com>
2023-10-01 00:38:47 -0500

    fix api-url

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
         b =>
         {
             b
-                .WithOrigins("https://leetify.live", "http://localhost:3000")
+                .WithOrigins(builder.Configuration["Jwt:Issuer"])
                 .AllowAnyHeader()
                 .AllowCredentials()
                 .AllowAnyMethod();
