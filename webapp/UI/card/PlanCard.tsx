2d6638b39a0d42d7cd7c9303d8e95e4adc37a2b9
lak <caizh1997@live.com>
2023-10-12 13:48:47 -0500

    limit height of plan card

--- webapp/UI/card/PlanCard.tsx
+++ webapp/UI/card/PlanCard.tsx
@@ -8,13 +8,34 @@ type Props = {
   userId?: string;
   heartClickable?: boolean;
 };
+
+function truncateString(str: string, maxLength: number) {
+  if (str.length <= maxLength) {
+    if (str.length < 32) {
+      return str + "\n"; // Add a line break if the string is shorter than 32 characters
+    } else {
+      return str;
+    }
+  } else {
+    return str.slice(0, maxLength - 3) + "...";
+  }
+}
+
+// Example usage:
+const shortString = "This is a short string.";
+const longString =
+  "This is a long string that needs to be truncated if it exceeds 20 characters.";
+
+console.log(truncateString(shortString, 20)); // Output: "This is a short string.\n"
+console.log(truncateString(longString, 20)); // Output: "This is a long strin..."
+
 export default function PlanCard({ plan, heartClickable = true }: Props) {
   const router = useRouter();
 
   return (
     <div
       onClick={() => router.push(`/plan/public/${plan.id}`)}
-      className="card w-80 rounded-lg relative min-w-90 shadow-xl cursor-pointer dark:bg-gray-800"
+      className="card max-h-40 w-80 rounded-lg relative min-w-90 shadow-xl cursor-pointer dark:bg-gray-800"
     >
       <div className="-z-0 min-w-md">
         <div className="absolute right-0 mr-8 mt-8">
@@ -27,7 +48,7 @@ export default function PlanCard({ plan, heartClickable = true }: Props) {
         </div>
         <div className="card-body">
           <h2 className="card-title">{plan.planName}</h2>
-          <p>{plan.description}</p>
+          <p>{truncateString(plan.description, 65)}</p>
           <div className="card-actions justify-end">
             {plan.tags && <TopicBadges topics={plan.tags} />}
           </div>
